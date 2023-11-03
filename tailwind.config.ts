a2830e00983fc3e4db7a59f4bac0bf2e54e62ddb
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-11-03 23:47:03 +0100

    💄 add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
+    fontSize: {
+      sm: "0.75 rem",
+      base: "1 rem",
+      xl: "1.25rem",
+      "2xl": "1.875rem",
+      "3xl": "2.5rem",
+      "4xl": "3.75rem",
+      "5xl": "5rem"
+    },
     container: {
       center: true,
-      padding: "2rem",
       screens: {
-        "2xl": "1400px",
-      },
+        "2xl": "1400px"
+      }
     },
     extend: {
       fontFamily: {
-        helvetica: ['var(--font-helvetica)'],
-        monument: ['var(--font-monument)'],
+        helvetica: ["var(--font-helvetica)"],
+        monument: ["var(--font-monument)"]
       },
       colors:{
         "neutral-0": "#ffffff",
         "neutral-1": "#f8f8f8",
         "neutral-2": "#343434",
         "neutral-3": "#161616",
-        "primary": "#da192a",
+        "primary": "#da192a"
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
