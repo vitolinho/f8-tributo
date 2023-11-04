088e840e5a72dc9fa2c7496f93e2c0dfcf0d2fca
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-11-04 01:07:51 +0100

    💄 add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Navbar from "@/components/navbar/navbar"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <div className="w-screen h-screen flex items-end">
+        <Image
+          alt="hero"
+          src={"/1.jpg"}
+          fill
+          objectFit="cover"
+          className="z-0 w-screen h-screen"
+        />
+        <div className="w-full flex justify-center items-center px-8 py-6 z-20 mix-blend-difference  lg:px-11 lg:py-[5.75rem] lg:justify-end">
+          <div className="heading-3 text-white mix-blend-difference lg:heading-1">f8 tributo</div>
+        </div>
+      </div>
+    </>
   )
 }
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,19 +5,21 @@ import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row justify-between items-center px-9 py-11 top-0 w-full lg:px-16">
-      <Link href={"/"}>
-        <Image
-          src={"/logo.svg"}
-          alt="logo"
-          width={30}
-          height={42}
-          className="mix-blend-difference"
-        />
-      </Link>
-      <div className="flex gap-x-10">
-        <a href="/" className="button text-white mix-blend-difference cursor-pointer">link 01</a>
-        <a href="/" className="button text-white mix-blend-difference cursor-pointer">link 02</a>
+    <div className="fixed top-0 w-screen z-40 mix-blend-difference px-9 py-11 lg:px-16">
+      <div className="flex flex-row justify-between items-center">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.svg"}
+            alt="logo"
+            width={30}
+            height={42}
+            className="mix-blend-difference"
+          />
+        </Link>
+        <div className="flex gap-x-10">
+          <a href="/" className="button text-white mix-blend-difference cursor-pointer">link 01</a>
+          <a href="/" className="button text-white mix-blend-difference cursor-pointer">link 02</a>
+        </div>
       </div>
     </div>
   )
